0dac7d6a8052c264058921c1b0013ea8087f99ec
Rayforth12 <56418246+Rayforth12@users.noreply.github.com>
2020-09-07 15:38:09 -0600

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,8 +1,21 @@
+import math
+
+
 def e_cuadratica(n):
-    
-    return 0
+    i=0
+    result = 0
+    while i<=n:
+    	factorial = math.factorial(i)
+    	result += 1/factorial
+    	i+=1
+    return result
 
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+    i=0
+    result = 0
+    while i<=n:
+    	result += 1/math.factorial(i)
+    	i+=1
+    return result
+    	
